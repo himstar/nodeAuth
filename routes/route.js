5b4fd1abc08715ce4789f0017f004290b829110c
Himanshu Mishra <Himanshu.Mishra@LCINDIA.LOCAL>
2018-05-07 16:47:21 +0530

    V3.1.4

--- routes/route.js
+++ routes/route.js
@@ -11,15 +11,25 @@ router.get('/users', (req, res, next)=> {
         res.json(users);
     });
 });
+router.post('/login', (req, res)=>{
+    var email = req.body.email;
+    var password = req.body.password;
+    User.findOne({ email: email, password: password }, (err, user)=> {
+        if (err) console.log(err);
 
+        if (user) {
+            res.json('Login succesfull');
+        } else {
+            res.json("invalidLogin");
+        }
+    });
+});
 router.post('/adduser', (req, res, next)=>{
-
     var name= req.body.name;
     var email= req.body.email;
     var phone= req.body.phone;
     var password= req.body.password;
     var assignedReviews= req.body.reviewId;
-
     User.findOne({ email: email }, (err, user)=>{
         if (err) console.log(err);
 
@@ -53,39 +63,18 @@ router.delete('/user/:id', (req, res, next)=>{
     });
 });
 
-router.post('/login', (req, res)=>{
-
-    var email = req.body.email;
-    var password = req.body.password;
-
-    User.findOne({ email: email, password: password }, (err, user)=> {
-        if (err) console.log(err);
-
-        if (user) {
-            res.json('Login succesfull');
-        } else {
-            res.json("invalidLogin");
-        }
-    });
-});
-
 //company
-
 router.get('/companies', (req, res, next)=> {
     Company.find((err, companies)=>{
         res.json(companies);
     });
 });
-
 router.post('/addcompany', (req, res, next)=> {
-
     var companyUrl= req.body.companyUrl;
     var companyName= req.body.companyName;
     var assignedReviews= req.body.reviewId;
-    
     Company.findOne({ companyUrl: companyUrl }, (err, company) => {
         if (err) console.log(err);
-
         if (company) {
             res.json("companyExists");
         } else {
@@ -113,20 +102,18 @@ router.delete('/company/:id', (req, res, next)=>{
         }
     });
 });
+
 // reviews
 router.get('/reviews', (req, res, next)=> {
     Review.find((err, reviews)=>{
         res.json(reviews);
     });
 });
-
-
 router.post('/addreview', (req, res, next)=> {
     var rate= req.body.rate;
     var review= req.body.review;
     var assignedUser= req.body.UserId;
     var assignedCompany= req.body.CompanyId;
-
     var newReview = new Review({
         rate: req.body.rate,
         review: req.body.review,
@@ -141,14 +128,21 @@ router.post('/addreview', (req, res, next)=> {
         }
     });    
 });
+router.delete('/review/:id', (req, res, next)=>{
+    Review.remove({_id: req.params.id}, (err, result)=>{
+        if(err){
+            res.json(err);
+        } else {
+            res.json('Review deleted successfully');
+        }
+    });
+});
 
 // middlewares
 // assign review to a user
 router.post('/assignreviewuser', (req, res, next)=>{
-
     var UserId = req.body.UserId;    
     var reviewId= req.body.reviewId;
-
     User.findOne({_id: UserId}, (err, user)=>{
         if (err) console.log(err);
         user.assignedReviews.push(reviewId);
@@ -161,9 +155,5 @@ router.post('/assignreviewuser', (req, res, next)=>{
         });
     });
 });
-
-
-
-
 
 module.exports = router;
